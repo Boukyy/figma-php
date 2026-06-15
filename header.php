e03e5ab9222960e097325220f8a9abd12d1830a4
Boukyy <40538@ma-web.nl>
2026-06-15 13:07:30 +0200

    Add header section for site branding and title

---
+++ header.php
@@ -0,0 +1,7 @@
+<header class="site-header">
+    <img class="logo" src="JAA_logo.svg" alt="JAA logo">
+    <div>
+        <h1>Duurzaam Huis Dashboard</h1>
+        <p>Monitor en beheer je energiezuinige woning</p>
+    </div>
+</header>
