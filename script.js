2ad484a2b9b46d6ab6424715b9d4f430a61960be
Boukyy <40538@ma-web.nl>
2026-06-27 10:25:29 +0200

    Refactor variable assignments for text content

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-document.getElementById("naam1").textContent = "Adam";
-document.getElementById("naam2").textContent = "Jayden";
-document.getElementById("naam3").textContent = "Averon";
+let naam1 = document.getElementById("naam1");
+let naam2 = document.getElementById("naam2");
+let naam3 = document.getElementById("naam3");
+
+naam1.textContent = "Adam";
+naam2.textContent = "Jayden";
+naam3.textContent = "Averon";
