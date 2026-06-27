f20dd4e555d8e543185a187780ec6f225fefcd30
Boukyy <40538@ma-web.nl>
2026-06-27 07:08:43 +0200

    Update description text in header

--- header.php
+++ header.php
@@ -2,6 +2,6 @@
     <img class="logo" src="JAA_logo.svg" alt="JAA logo">
     <div>
         <h1>Duurzaam Huis Dashboard</h1>
-        <p>Monitor en beheer onze energiezuinige woning</p>
+        <p>Monitor en beheer een energiezuinige woning.</p>
     </div>
 </header>
